ff26cada993086170444be16f9d2b55d6e75579c
DebAxom <debaxom@gmail.com>
2023-03-13 18:17:35 +0530

    Added the functionality to change route on <a> tags dynamically

--- compiler/template/n-route.js
+++ compiler/template/n-route.js
@@ -2,7 +2,7 @@ function Route(VirtualDocument){
     VirtualDocument.window.document.body.querySelectorAll('a[n:route]').forEach(child=>{
         let route = child.getAttribute('n:route');
         child.removeAttribute('n:route');
-        child.setAttribute('onclick',`return window.nijor.redirect('this.href')`);
+        child.setAttribute('onclick',`return window.nijor.redirect(this.href)`);
         child.setAttribute('href',route);
     });
     return VirtualDocument.window.document.body.innerHTML;
