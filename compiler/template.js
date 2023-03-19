86378735873a5e614ed68e4c2c9f1c85f586a1ad
DebAxom <debaxom@gmail.com>
2023-03-19 16:37:38 +0530

    Fixed a minor bug

--- compiler/template.js
+++ compiler/template.js
@@ -101,7 +101,7 @@ module.exports = function(doc,scope,ComponentScope,options,specsAttr){
             element.setAttribute('data-n:asyncLoad',eventName);
         }
         
-        element.setAttribute('on:'+eventName,fnName+`(this,$\{${specsAttr}})`);
+        element.setAttribute('on:'+eventName,fnName+`(this,$data)`);
         
         element.querySelectorAll('*').forEach(child=>{
             let elementName = child.tagName.toLowerCase();
